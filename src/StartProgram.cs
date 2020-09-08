585a42df47420ae533959f1e485df70c026f3bcd
Amal kuriakose <67690738+nolifeasian24-7@users.noreply.github.com>
2020-09-08 22:12:03 +0100

    Add files via upload
    Visual Cs varient, i practiced working with Console and file handling (not completed, im struggling with reading, however the code will generate a document in a specified file location. Please look at the picture included with the newest commit to date (08/09/2020)
    It is line 23 of the getFood file, please change the location in quotation marks to the desired file location.

---
+++ src/StartProgram.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TimsDinercs
+{
+    class startProgram
+    {
+        static void Main(string[] args)
+        {
+            Program startProgram = new Program();
+            startProgram.table();
+        }
+    }
+}
